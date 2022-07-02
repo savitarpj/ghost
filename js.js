2a187d6428c85ce4e0a07a26b2999ef80efe4e15
ArmTanks <105069746+savitarpj@users.noreply.github.com>
2022-07-03 02:18:12 +0400

    undefined

---
+++ js.js
@@ -0,0 +1,17 @@
+    
+    
+    const myFunction = name=> {
+        return `Ghost is ${name}`;
+    }
+
+    console.log (myFunction('Tyom')) // Output Ghost is Tyom
+
+    // Or even without using arrow or implement `return` keyword
+    const myFunctionTwo = name => `Ghost is ${name}`
+
+    console.log (myFunctionTwo('Tyom')) // Output Ghost is Tyom 
+
+
+
+
+
