db8a915739b0559f99409215cb32bf8817f7e32d
ArmTanks <tyomht@mail.ru>
2022-07-10 20:56:51 +0400

    undefined

---
+++ assets/js/pawno.js
@@ -0,0 +1,26 @@
+const
+
+consoleAttention
+
+ =
+
+ `      
+                                      Attention! 
+                It is not recommended to use codes you will only harm yourself
+                
+`
+
+
+console
+
+.
+
+log
+
+(
+
+consoleAttention
+
+)
+
+;
